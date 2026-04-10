e21f5a89a03aef424f87e7f0e77db86852adb652
mushi0ma <contact.mushi0ma@gmail.com>
2026-04-10 19:56:29 +0500

    fix: trying to fix network connection

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -36,6 +36,6 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001, '0.0.0.0');
 }
 bootstrap();
